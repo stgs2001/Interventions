83ae60d173e4279a58b63d7826a2ceec9acf7f9a
stgs2001 <stgs2001@hotmail.com>
2020-04-30 17:34:49 -0400

    TP6 premiere version

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AccueilComponent } from './accueil/accueil.component';
+import { ProblemeComponent } from './probleme/probleme.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'accueil', component:AccueilComponent},
+  { path:'', redirectTo:'accueil', pathMatch:'full'},
+  { path:'**', redirectTo:'accueil', pathMatch:'full'}, //si la route est inexistante, rediriger l'utilisateur vers Accueil
+
+  { path:'probleme', component:ProblemeComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,20 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { AccueilComponent } from './accueil/accueil.component';
+import { ProblemeComponent } from './probleme/probleme.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccueilComponent,
+    ProblemeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FontAwesomeModule
+    FontAwesomeModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
