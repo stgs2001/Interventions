52155436c5593bb49d5fc8473857ece2ee8c2372
stgs2001 <stgs2001@hotmail.com>
2020-04-30 20:46:08 -0400

    TP7 test6 (tous les tests sont complétés et fonctionnent)

--- src/app/probleme/probleme.component.spec.ts
+++ src/app/probleme/probleme.component.spec.ts
@@ -50,4 +50,10 @@ describe('ProblemeComponent', () => {
     zone.setValue(' '.repeat(10));
     expect(zone.valid).toBeTruthy();
   });
+
+  it('Zone PRÉNOM valide avec 2 espaces et 1 caractère', () => {
+    let zone = component.problemeForm.controls['nomProbleme'];
+    zone.setValue("  a");
+    expect(zone.valid).toBeTruthy();
+  });
 });
